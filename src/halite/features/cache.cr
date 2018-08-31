7f999538073b6e02e59dc53951073ca90d3e5c72
icyleaf <icyleaf.cn@gmail.com>
2018-08-31 18:42:02 +0800

    feat(cache): store cache into a directory with metdata file and body file

--- src/halite/features/cache.cr
+++ src/halite/features/cache.cr
@@ -19,9 +19,9 @@ module Halite
   # - `X-Cached-By`: Always return "Halite"
   #
   # ```
-  # Halite.use("cache").get "http://httpbin.org/anything"         # request a HTTP
-  # r = Halite.use("cache").get "http://httpbin.org/anything"     # request from local storage
-  # r.headers # => {..., "X-Cached-At" => "2018-08-30 10:41:14 UTC", "X-Cached-By" => "Halite", "X-Cached-Expires-At" => "2018-08-30 10:41:19 UTC", "X-Cached-Key" => "2bb155e6c8c47627da3d91834eb4249a"}}
+  # Halite.use("cache").get "http://httpbin.org/anything"     # request a HTTP
+  # r = Halite.use("cache").get "http://httpbin.org/anything" # request from local storage
+  # r.headers                                                 # => {..., "X-Cached-At" => "2018-08-30 10:41:14 UTC", "X-Cached-By" => "Halite", "X-Cached-Expires-At" => "2018-08-30 10:41:19 UTC", "X-Cached-Key" => "2bb155e6c8c47627da3d91834eb4249a"}}
   # ```
   class Cache < Feature
     DEFAULT_PATH = "cache/"
@@ -31,9 +31,16 @@ module Halite
     @debug : Bool
 
     def initialize(**options)
-      @path = options.fetch(:path, DEFAULT_PATH).as(String)
-      @expires = options[:expires]?.as(Time::Span?)
       @debug = options.fetch(:debug, true).as(Bool)
+      @path = options.fetch(:path, DEFAULT_PATH).as(String)
+      @expires = case expires = options[:expires]?
+                 when Time::Span
+                   expires.as(Time::Span)
+                 when Int32
+                   Time::Span.new(seconds: expires.as(Int32), nanoseconds: 0)
+                 else
+                   raise "Only accept Int32 and Time::Span type."
+                 end
     end
 
     def intercept(chain)
@@ -56,24 +63,35 @@ module Halite
 
     private def find_cache(request : Request) : Response?
       key = generate_cache_key(request)
-      file = File.join(@path, key)
+      path = File.join(@path, key)
+      file = File.join(path, "#{key}.cache")
 
       if File.exists?(file) && !cache_expired?(file)
-        cache = JSON.parse(File.open(file)).as_h
-        status_code = cache["status_code"].as_i
-        body = cache["body"].as_s
-        headers = cache["headers"].as_h.each_with_object(HTTP::Headers.new) do |(key, value), obj|
-          obj[key] = value.as_s
+        status_code = 200
+        headers = HTTP::Headers.new
+        if metadata = find_metadata(path)
+          status_code = metadata["status_code"].as_i
+          metadata["headers"].as_h.each do |key, value|
+            headers[key] = value.as_s
+          end
+
+          if @debug
+            headers["X-Cached-Key"] = key
+            headers["X-Cached-At"] = cache_created_time(file).to_s
+            headers["X-Cached-Expires-At"] = @expires ? (cache_created_time(file) + @expires.not_nil!).to_s : "None"
+            headers["X-Cached-By"] = "Halite"
+          end
         end
 
-        if @debug
-          headers["X-Cached-Key"] = key
-          headers["X-Cached-At"] = cache_created_time(file).to_s
-          headers["X-Cached-Expires-At"] = @expires ? (cache_created_time(file) + @expires.not_nil!).to_s : "None"
-          headers["X-Cached-By"] = "Halite"
-        end
-
+        body = File.read_lines(file).join("\n")
         return Response.new(request.uri, status_code, body, headers)
+      end
+    end
+
+    private def find_metadata(path)
+      file = File.join(path, "metadata.json")
+      if File.exists?(file)
+        JSON.parse(File.open(file)).as_h
       end
     end
 
@@ -92,15 +110,27 @@ module Halite
     end
 
     private def write_cache(request, response)
-      FileUtils.mkdir_p(@path) unless Dir.exists?(@path)
-
       key = generate_cache_key(request)
-      File.open(File.join(@path, key), "w") do |f|
+      path = File.join(@path, key)
+
+      FileUtils.mkdir_p(path) unless Dir.exists?(path)
+
+      write_metadata(path, response)
+      write_body(path, key, response)
+    end
+
+    private def write_metadata(path, response)
+      File.open(File.join(path, "metadata.json"), "w") do |f|
         f.puts({
           "status_code" => response.status_code,
-          "headers" => response.headers.to_h,
-          "body" => response.body
+          "headers"     => response.headers.to_h,
         }.to_json)
+      end
+    end
+
+    private def write_body(path, key, response)
+      File.open(File.join(path, "#{key}.cache"), "w") do |f|
+        f.puts response.body
       end
     end
 
