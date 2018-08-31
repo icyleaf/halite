478f087363739c5c55970dd37f6d569e1f5b29fc
icyleaf <icyleaf.cn@gmail.com>
2018-08-31 17:16:06 +0800

    test: fix ci

--- spec/halite/features/logging_spec.cr
+++ spec/halite/features/logging_spec.cr
@@ -1,9 +1,18 @@
 require "../../spec_helper"
 
+private class NulleLogger < Halite::Logging::Abstract
+  def request(request)
+  end
+
+  def response(response)
+  end
+end
+
 describe Halite::Logging do
   it "should register a format" do
-    Halite::Logging["simple"].should eq(SimpleLogger)
-    Halite::Logging.availables.should eq ["common", "json", "simple"]
+    Halite::Logging.register "null", NulleLogger
+    Halite::Logging.availables.includes?("null").should be_true
+    Halite::Logging["null"].should eq(NulleLogger)
   end
 
   it "should use common as default logger" do
@@ -16,8 +25,8 @@ describe Halite::Logging do
   end
 
   it "should use custom logger" do
-    logger = Halite::Logging.new(logger: SimpleLogger.new)
-    logger.writer.should be_a(SimpleLogger)
+    logger = Halite::Logging.new(logger: NulleLogger.new)
+    logger.writer.should be_a(NulleLogger)
     logger.writer.skip_request_body.should be_false
     logger.writer.skip_response_body.should be_false
     logger.writer.skip_benchmark.should be_false
