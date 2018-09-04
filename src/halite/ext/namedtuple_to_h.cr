c75db3df6a8df7147f930526c1b392dfa18ae8c8
icyleaf <icyleaf.cn@gmail.com>
2018-09-04 16:05:19 +0800

    refactor(ext): return empty hash for an empty named tuple

--- src/halite/ext/namedtuple_to_h.cr
+++ src/halite/ext/namedtuple_to_h.cr
@@ -3,7 +3,7 @@ struct NamedTuple
   #
   # TODO: This is fix bug, It will remove if PR is merged https://github.com/crystal-lang/crystal/pull/6628
   def to_h
-    raise "Can't convert an empty NamedTuple to a Hash" if empty?
+    # raise "Can't convert an empty NamedTuple to a Hash" if empty?
 
     {% if T.size > 0 %}
     {
@@ -11,6 +11,8 @@ struct NamedTuple
         {{key.symbolize}} => self[{{key.symbolize}}],
       {% end %}
     }
+    {% else %}
+      {} of NoReturn => NoReturn
     {% end %}
   end
 end
