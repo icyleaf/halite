d888b828aacc4219e3efda4f620091c27789dec4
icyleaf <icyleaf.cn@gmail.com>
2018-09-11 11:27:50 +0800

    doc: update named tuple extension todo

--- src/halite/ext/namedtuple_to_h.cr
+++ src/halite/ext/namedtuple_to_h.cr
@@ -1,16 +1,14 @@
 struct NamedTuple
   # Returns a `Hash` with the keys and values in this named tuple.
   #
-  # TODO: This is fix bug, It will remove if PR is merged https://github.com/crystal-lang/crystal/pull/6628
+  # TODO: Remove it when Crystal 0.27.0 is released. https://github.com/crystal-lang/crystal/pull/6628
   def to_h
-    # raise "Can't convert an empty NamedTuple to a Hash" if empty?
-
     {% if T.size > 0 %}
-    {
-      {% for key in T %}
-        {{key.symbolize}} => self[{{key.symbolize}}],
-      {% end %}
-    }
+      {
+        {% for key in T %}
+          {{key.symbolize}} => self[{{key.symbolize}}],
+        {% end %}
+      }
     {% else %}
       {} of NoReturn => NoReturn
     {% end %}
